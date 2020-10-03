a2ac887847a2491454aa0f961b089e0a2ed08ab0
wiiworld <xile803@likelion.org>
2020-10-03 13:40:18 +0900

    add : chore: g

---
+++ src/views/js/mongo.js
@@ -0,0 +1,1 @@
+var mongoose = require('mongoose')var db = mongoose.connection;db.on('error', console.error);db.once('open', function(){    console.log("Connected to mongod server")})mongoose.connect('mongodb://localhost/mongodb_tutorial')
